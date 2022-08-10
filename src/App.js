da1ddd08dee508c8f0e07c86a79fa77ce6d7e53c
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-08-10 23:23:51 +0200

    Add elint disable rule.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
+/* eslint-disable */
 import Calculator from './components/Calculator';
+/* eslint-enable */
 
 class App extends React.Component {
   constructor(props) {
