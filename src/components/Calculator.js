e572e374ebf71013ad36b6979b2a44b206487c76
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-08-11 22:05:45 +0200

    Fix eslint errors.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import './calculatorStyles.css';
 import calculate from '../logic/calculate';
+
 const Calculator = () => {
   const [obj, setObj] = useState({ total: 0, next: null, operation: null });
   const handleClick = (e) => {
     setObj(calculate(obj, e.target.id));
   };
-  
+
   const { total, next, operation } = obj;
   return (
     <div className="calculator">
@@ -14,29 +15,28 @@ const Calculator = () => {
         <span>{total}</span>
         <span>{operation}</span>
         <span>{next}</span>
-        </h2>
-        <button type="button" onClick={this.handleClick} id="AC" className="c-item">AC</button>
-        <button type="button" onClick={this.handleClick} id="+/-" className="c-item">+/-</button>
-        <button type="button" onClick={this.handleClick} id="%" className="c-item">%</button>
-        <button type="button" onClick={this.handleClick} id="÷" className="c-item bg-orange">÷</button>
-        <button type="button" onClick={this.handleClick} id="7" className="c-item">7</button>
-        <button type="button" onClick={this.handleClick} id="8" className="c-item">8</button>
-        <button type="button" onClick={this.handleClick} id="9" className="c-item">9</button>
-        <button type="button" onClick={this.handleClick} id="x" className="c-item bg-orange">x</button>
-        <button type="button" onClick={this.handleClick} id="4" className="c-item">4</button>
-        <button type="button" onClick={this.handleClick} id="5" className="c-item">5</button>
-        <button type="button" onClick={this.handleClick} id="6" className="c-item">6</button>
-        <button type="button" onClick={this.handleClick} id="-" className="c-item bg-orange">-</button>
-        <button type="button" onClick={this.handleClick} id="1" className="c-item">1</button>
-        <button type="button" onClick={this.handleClick} id="2" className="c-item">2</button>
-        <button type="button" onClick={this.handleClick} id="3" className="c-item">3</button>
-        <button type="button" onClick={this.handleClick} id="+" className="c-item bg-orange">+</button>
-        <button type="button" onClick={this.handleClick} id="0" className="c-item zero">0</button>
-        <button type="button" onClick={this.handleClick} id="." className="c-item">.</button>
-        <button type="button" onClick={this.handleClick} id="=" className="c-item bg-orange">=</button>
-      </div>
-    );
-  }
-
+      </h2>
+      <button type="button" onClick={handleClick} id="AC" className="c-item">AC</button>
+      <button type="button" onClick={handleClick} id="+/-" className="c-item">+/-</button>
+      <button type="button" onClick={handleClick} id="%" className="c-item">%</button>
+      <button type="button" onClick={handleClick} id="÷" className="c-item bg-orange">÷</button>
+      <button type="button" onClick={handleClick} id="7" className="c-item">7</button>
+      <button type="button" onClick={handleClick} id="8" className="c-item">8</button>
+      <button type="button" onClick={handleClick} id="9" className="c-item">9</button>
+      <button type="button" onClick={handleClick} id="x" className="c-item bg-orange">x</button>
+      <button type="button" onClick={handleClick} id="4" className="c-item">4</button>
+      <button type="button" onClick={handleClick} id="5" className="c-item">5</button>
+      <button type="button" onClick={handleClick} id="6" className="c-item">6</button>
+      <button type="button" onClick={handleClick} id="-" className="c-item bg-orange">-</button>
+      <button type="button" onClick={handleClick} id="1" className="c-item">1</button>
+      <button type="button" onClick={handleClick} id="2" className="c-item">2</button>
+      <button type="button" onClick={handleClick} id="3" className="c-item">3</button>
+      <button type="button" onClick={handleClick} id="+" className="c-item bg-orange">+</button>
+      <button type="button" onClick={handleClick} id="0" className="c-item zero">0</button>
+      <button type="button" onClick={handleClick} id="." className="c-item">.</button>
+      <button type="button" onClick={handleClick} id="=" className="c-item bg-orange">=</button>
+    </div>
+  );
+};
 
 export default Calculator;
