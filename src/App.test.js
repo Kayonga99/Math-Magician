a3ab5897ab8eea8ff5155527e42eb3eb294c34b2
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-08-18 17:50:03 +0200

    Update path.

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 import Calculator from './components/Calculator';
 import Home from './components/home';
-import NavBar from './components/navBar';
+import NavBar from './components/navbar';
 import Quotes from './components/quote';
 import calculate from './logic/calculate';
 import operate from './logic/operate';
